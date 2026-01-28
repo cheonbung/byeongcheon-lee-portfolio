5ee17b0379c9784d8e9cebbeac5371ea9cfa4deb
cheonbung <lbcheon0208@gmail.com>
2026-01-28 15:54:04 +0900

    Fix white screen by updating vite base path and ensure Naver verification tag

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/byeongcheon-lee-portfolio/',
+  // 리포지토리 이름 변경에 따라 base 경로를 루트('/')로 설정합니다.
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     }
   }
-  // define 부분은 삭제했습니다.
 });
